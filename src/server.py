5a2e3d7d0e6407ad3150dd9de7f78c4d30dff2fb
Jonas Meng <jonas-meng@users.noreply.github.com>
2021-11-07 21:46:06 +0100

    seperate data based on collection contract address

--- src/server.py
+++ src/server.py
@@ -6,23 +6,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def get_entries(exchange):
+def get_entries(exchange, contract_address):
     nft_data = db.getDb(os.path.realpath("db.json"))
     data = nft_data.getByQuery(query={
         "exchange": exchange
     })
     data = sorted(data, key=lambda x: x["timestamp"])
     if exchange == "nftrade":
-        price_list = [float(item["data"]["price"]) for item in data]
-        contract_address = data[0]["data"]["contractAddress"]
+        price_list = [float(item["data"]["price"]) for item in data if item["data"]["contractAddress"] == contract_address]
         web_link = f"https://app.nftrade.com/assets/bsc/{contract_address}"
     elif exchange == "pancakeswap":
-        price_list = [float(item["data"]["currentAskPrice"]) for item in data]
-        contract_address = data[0]["data"]["collection"]["id"]
+        price_list = [float(item["data"]["currentAskPrice"]) for item in data if item["data"]["collection"]["id"] == contract_address]
         web_link = f"https://pancakeswap.finance/nfts/collections/{contract_address}"
     else:
         price_list = []
-        contract_address = ""
         web_link = ""
     timestamps = [item["timestamp"] for item in data]
     return {
@@ -36,7 +33,12 @@ def get_entries(exchange):
 
 @app.route("/")
 def index():
-    chart_data_list = [get_entries("nftrade"), get_entries("pancakeswap")]
+    chart_data_list = [
+        get_entries("nftrade", "0x57a7c5d10c3f87f5617ac1c60da60082e44d539e"),
+        get_entries("pancakeswap", "0x0a8901b0e25deb55a87524f0cc164e9644020eba"),
+        get_entries("pancakeswap", "0xdf7952b35f24acf7fc0487d01c8d5690a60dba07"),
+        get_entries("pancakeswap", "0x3da8410e6ef658c06e277a2769816688c37496cf")
+    ]
     return render_template("index.html", chart_data_list=chart_data_list)
 
 
